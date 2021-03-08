315d5def74cddfb58bd0ee2a42487ef5f22d1c3b
Mauricio Foronda <josemauricioforonda@gmail.com>
2021-03-08 01:04:32 -0400

    map

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -7,7 +7,7 @@ import csv
 import os
 
 def download(departamento):
-
+    
     headers = {
         'authority': 'computo.oep.org.bo',
         'pragma': 'no-cache',
@@ -26,19 +26,24 @@ def download(departamento):
     }
 
     dep_id = departamento['id']
+    nuevo = 0
 
     archivo = requests.post('https://computo.oep.org.bo/api/v1/descargar', headers=headers, data=f'{{"tipoArchivo":"CSV", "idDepartamento":{dep_id}}}')
     response = requests.get(archivo.json()['datoAdicional']['archivo'])
     csvdata = csv.reader(response.content.decode('iso8859').splitlines(), delimiter='|')
     data = [row for row in csvdata]
     if len(data) > 0:
-        filename = 'datos/{}/{}.csv'.format(departamento['nombre'], '_'.join(archivo.json()['datoAdicional']['archivo'].split('/')[-1].split('_')[1:4]))
-        df = pd.DataFrame(data[1:], columns=data[0])
-        if not os.path.exists('datos'):
-            os.makedirs('datos')
-        if not os.path.exists('datos/{}'.format(departamento['nombre'])):
-            os.makedirs('datos/{}'.format(departamento['nombre']))
-        df.to_csv(filename, index=False, encoding='utf-8')
+        filename = '_'.join(archivo.json()['datoAdicional']['archivo'].split('/')[-1].split('_')[1:4]) + '.csv'
+        if os.listdir('datos/{}/'.format(departamento['nombre']))[-1] != filename:
+            filename_complete = 'datos/{}/{}'.format(departamento['nombre'], filename)
+            df = pd.DataFrame(data[1:], columns=data[0])
+            if not os.path.exists('datos'):
+                os.makedirs('datos')
+            if not os.path.exists('datos/{}'.format(departamento['nombre'])):
+                os.makedirs('datos/{}'.format(departamento['nombre']))
+            df.to_csv(filename_complete, index=False, encoding='utf-8')
+            nuevo = 1
+    return nuevo
 
 departamentos = [
     {'nombre':'chuquisaca', 'id':1},
@@ -51,5 +56,10 @@ departamentos = [
     {'nombre':'beni', 'id':8},
     {'nombre':'pando', 'id':9}
 ]
+nuevos = []
 for departamento in departamentos:
-    download(departamento)
+    nuevos.append(download(departamento))
+if sum(nuevos) > 0:
+    print(1)
+else:
+    print(0)
